3c40be583b4c4bc0d5c86945a4b29093fcaa4d97
Allen <allenthomas204@gmail.com>
2023-06-04 14:10:48 +0530

    version 2

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
-
 import os
 import pytube
-#from http.client import IncompleteRead
 
-# Replace PLAYLIST_URL with the URL of the YouTube playlist
-# Replace START_VIDEO with the number of the first video you want to download
-# (e.g. if you want to start downloading from the third video, set START_VIDEO = 3)
-PLAYLIST_URL = "ENTER THE URL OF YOUTUBE PLAYLIST HERE"
- 
-START_VIDEO = 1 
-END_VIDEO=50
+# enter the YouTube playlist URL
+PLAYLIST_URL = input("Enter the YouTube playlist URL: ")
+
+# enter the start and end video numbers
+START_VIDEO = int(input("Enter the start video number: "))
+END_VIDEO = int(input("Enter the end video number: "))
+
+# enter the output directory
+output_dir = input("Enter the output directory: ")
 
 # Create a PyTube playlist object
 playlist = pytube.Playlist(PLAYLIST_URL)
@@ -17,40 +17,31 @@ playlist = pytube.Playlist(PLAYLIST_URL)
 # Get a list of all the video URLs in the playlist
 video_urls = playlist.video_urls
 
-# Iterate over the list of video URLs starting from the specified video number
+# Iterate over the list of video URLs 
 for url in video_urls[START_VIDEO - 1:END_VIDEO]:
     # Create a PyTube video object
     video = pytube.YouTube(url)
 
-    # Get the video's title and file size
-    video_title = video.title
     try:
-        file_size = video.streams.first().filesize
-    except KeyError:
-        file_size = "unknown"
-    #file_size = video.streams.first().filesize
-    
-    # Print the video's title and file size
-    print(f"Downloading '{video_title}' ...")
+        # Get the video's title
+        video_title = video.title
+    except pytube.exceptions.PytubeError:
+        # If error occurs, fallback title
+        video_title = "Unknown_Title"
 
-    # Set the output directory for the downloaded videos
-    # Replace "videos" with the desired output directory
-    output_dir = "cpp"
+    # Print the video's title
+    print(f"Downloading '{video_title}' ...")
 
     # Check if the output directory exists, and create it if it doesn't
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
     # Try to get a 720p video stream
-    video_stream = video.streams.filter(resolution='720p').first()
+    video_stream = video.streams.get_highest_resolution()
 
-    # If no 720p stream is available, try to get a 480p stream
+    # If no 720p stream is available, try to get the highest resolution stream
     if video_stream is None:
-        video_stream = video.streams.filter(resolution='480p').first()
-
-    # If no 480p stream is available, try to get a 360p stream
-    if video_stream is None:
-        video_stream = video.streams.filter(resolution='360p').first()
+        video_stream = video.streams.get_highest_resolution()
 
     # Download the video to the output directory
     video_stream.download(output_dir)
